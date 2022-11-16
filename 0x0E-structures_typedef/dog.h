fcc8afc13293d1a8ea8955832a76420e259cf951
orueginren <ijawboi360@gmail.com>
2022-11-15 19:07:31 -0800

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,18 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * description - a struct dog code
- *
- * Return: Always 0.
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
+
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
+#endif
